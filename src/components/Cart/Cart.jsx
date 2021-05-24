6c1b025244ca755e152d3da9176e48e2247e6a37
Adrian Chaves Ghilardi <adrian.chaves@gmail.com>
2021-05-24 20:08:34 -0300

    firebase 2

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,22 +1,47 @@
-import React, {useContext, useEffect} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import { CartContext } from "../Context/Context";
+import { getFirestore } from '../Firebase/'
+
 
 
 export default function Cart() {
-const [products, productsCount, addProduct, delProduct, getGrandTotal] = useContext(CartContext);
+  const [products, productsCount, addProduct, delProduct, getGrandTotal] = useContext(CartContext);
+  const [OrderId, setOrderId] = useState(0);
+  const handleClick = () => {
+    const db = getFirestore();
+    const order = db.collection("order");
+    const newOrder = {
+      buyer: 'adrian',
+      items: ''
+  
+    };
+    order.add(newOrder).then(({id}) => {
+      setOrderId(id);
+    } ).catch(err =>{
+      console.log("error" + err);
+    });
+  } 
+
   useEffect(()=>{
     console.log(products)
   })
-  return <div>Bienvenidos al 
-  cart
-          {products.map((product) => (
-          <div>
-          <hr></hr>
-            <p>Item:{product.nombre}</p>
-            <p>Cantidad:{product.quantity}</p>
-            <hr></hr>
-          </div>
-        ))}
-  </div>;
+
+  return (
+    <div>
+      <h1>
+        Bienvenidos al Cart
+      </h1>
+      {products.map((p) => (
+        <div>
+          <p>Item: {p.nombre}</p>
+          <p>Cantidad: {p.quantity}</p>
+
+        </div>
+      ))
+      }
+      <button onClick={handleClick}>Comprar</button>
+    </div>
+  )
+
 
 }
